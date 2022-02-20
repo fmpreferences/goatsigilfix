8bda1b137629321946b7ba480565c1faf584f694
fmpreferences <57734230+fmpreferences@users.noreply.github.com>
2022-02-20 18:03:34 -0500

    Added config

--- Class1.cs
+++ Class1.cs
@@ -1,4 +1,5 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
 using BepInEx.Logging;
 using DiskCardGame;
 using HarmonyLib;
@@ -8,18 +9,33 @@ namespace GoatSigilAlwaysBoneLord;
 [BepInPlugin(PluginGuid, PluginName, PluginVersion)]
 public class GoatSigilFix : BaseUnityPlugin
 {
-    private const string PluginName = "Goat Sigil Fix";
+    private const string PluginName = "GoatSigilFix";
     private const string PluginGuid = "fmpreferences.goatfix";
     private const string PluginVersion = "1.0.0";
 
     internal static ManualLogSource PluginLogger;
+    internal static bool allowTransfers;
 
     private void Awake()
     {
         PluginLogger = Logger;
-        PluginLogger.LogInfo("Goat Sigil Fix loaded!");
 
-        new Harmony(PluginGuid).PatchAll();
+        var h = new Harmony(PluginGuid);
+
+        allowTransfers = Config.Bind(PluginName, "allowTransferredSigils", true,
+                new ConfigDescription("Will give bone lord boon even if the sigil is transferred via sacrifice"))
+            .Value;
+
+        PluginLogger.LogInfo("Config Options Loaded!");
+        try
+        {
+            h.PatchAll();
+            PluginLogger.LogInfo($"{PluginName} loaded!");
+        }
+        catch
+        {
+            PluginLogger.LogInfo("something went wrong!");
+        }
     }
 }
 
@@ -28,20 +44,25 @@ public class CanIAddGoat
 {
     /*
      * what this thing does is add the goat trait to anything with the triple blood sigil and makes it
-     * have the goat trait which makes the sacrifice give the bone lord boon. I was way overcomplicating
-     * the code
+     * have the goat trait which makes the sacrifice give the bone lord boon.
      */
     [HarmonyPrefix]
     public static void PatchValidCards()
     {
         var deckInfo = RunState.Run.playerDeck;
-        List<CardInfo> cards = deckInfo.Cards.FindAll(card => card.HasAbility(Ability.TripleBlood));
-        foreach (var card in cards)
-        {
+        List<CardInfo> defaultGoats =
+            deckInfo.Cards.FindAll(card => card.DefaultAbilities.Contains(Ability.TripleBlood));
+
+        foreach (var card in defaultGoats)
             if (!card.traits.Contains(Trait.Goat))
-            {
                 card.traits.Add(Trait.Goat);
-            }
-        }
+
+        if (!GoatSigilFix.allowTransfers) return;
+        
+        List<CardInfo> transferredGoats =
+            deckInfo.Cards.FindAll(card => card.ModAbilities.Contains(Ability.TripleBlood));
+        foreach (var card in transferredGoats)
+            if (!card.traits.Contains(Trait.Goat))
+                card.traits.Add(Trait.Goat);
     }
 }
