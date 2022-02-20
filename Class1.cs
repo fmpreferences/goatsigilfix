472bc1789f0e0ca145b0a486fdcfb2e721348e4b
fmpreferences <57734230+fmpreferences@users.noreply.github.com>
2022-02-20 00:45:32 -0500

    initial commit

---
+++ Class1.cs
@@ -0,0 +1,47 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using DiskCardGame;
+using HarmonyLib;
+
+namespace GoatSigilAlways4Bones;
+
+[BepInPlugin(PluginGuid, PluginName, PluginVersion)]
+public class GoatSigilFix : BaseUnityPlugin
+{
+    private const string PluginName = "Goat Sigil Fix";
+    private const string PluginGuid = "fmpreferences.goatfix";
+    private const string PluginVersion = "0.0.1";
+
+    internal static ManualLogSource PluginLogger;
+
+    private void Awake()
+    {
+        PluginLogger = Logger;
+        PluginLogger.LogInfo("Goat Sigil Fix loaded!");
+
+        new Harmony(PluginGuid).PatchAll();
+    }
+}
+
+[HarmonyPatch(typeof(CardRemoveSequencer), "GetValidCards")]
+public class CanIAddGoat
+{
+    /*
+     * what this thing does is add the goat trait to anything with the triple blood sigil and makes it
+     * have the goat trait which makes the sacrifice give the bone lord boon. I was way overcomplicating
+     * the code
+     */
+    [HarmonyPrefix]
+    public static void PatchValidCards()
+    {
+        var deckInfo = RunState.Run.playerDeck;
+        List<CardInfo> cards = deckInfo.Cards.FindAll(card => card.HasAbility(Ability.TripleBlood));
+        foreach (var card in cards)
+        {
+            if (!card.traits.Contains(Trait.Goat))
+            {
+                card.traits.Add(Trait.Goat);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # goatsigilfix
 Any time you sacrifice a triple blood sigil to the bone lord, it will give you the bone lord boon regardless of what card it is or if it was a sacrifice.
+
